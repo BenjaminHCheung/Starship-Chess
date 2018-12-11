400df4d5642e7cb7deac07a069b196fbc2079b59
Benjamin Cheung <benjaminhcheung@gmail.com>
2018-12-10 22:29:01 -0700

    added basic functionality to the header of ship update callback

--- shipupdatecallback.h
+++ shipupdatecallback.h
@@ -1,11 +1,19 @@
 #ifndef SHIPUPDATECALLBACK_H
 #define SHIPUPDATECALLBACK_H
 
+#include <osg/PositionAttitudeTransform>
+#include <osg/NodeVisitor>
+#include <osg/Node>
+#include "starship.h"
 
-class ShipUpdateCallback
+class ShipUpdateCallback: public osg::NodeCallback
 {
 public:
-    ShipUpdateCallback();
+    ShipUpdateCallback(Starship* starship);
+    virtual void operator()(osg::Node* node, osg::NodeVisitor* nodevisitor);
+
+protected:
+    Starship* mStarship;
 };
 
 #endif // SHIPUPDATECALLBACK_H
